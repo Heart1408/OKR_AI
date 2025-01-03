661449e229187028808ae54e132366d086cb2ad2
tamvt <vuthitam14082001@gmail.com>
2025-01-03 13:10:21 +0700

    update

--- src/base/llm_model.py
+++ src/base/llm_model.py
@@ -10,17 +10,17 @@ MODEL = os.getenv("OPENAI_MODEL") or os.getenv("GOOGLE_MODEL")
 EMBEDDING_MODEL = os.getenv("OPENAI_EMBEDDING_MODEL") or os.getenv("GOOGLE_EMBEDDING_MODEL")
 
 def get_llm():
-    # if os.getenv("OPENAI_API_KEY"):
-    llm = ChatOpenAI(model=MODEL, openai_api_key=API_KEY, temperature=0.0)
-    # else:
-    #     llm = ChatGoogleGenerativeAI(model=MODEL, google_api_key=API_KEY, template=0.0)
+    if os.getenv("OPENAI_API_KEY"):
+        llm = ChatOpenAI(model=MODEL, openai_api_key=API_KEY, temperature=0.0)
+    else:
+        llm = ChatGoogleGenerativeAI(model=MODEL, google_api_key=API_KEY, template=0.0)
 
     return llm
 
 def get_model_embedding():
-    # if os.getenv("OPENAI_API_KEY"):
-    model = OpenAIEmbeddings(model=EMBEDDING_MODEL)
-    # else:
-    #     model = GoogleGenerativeAIEmbeddings(model=EMBEDDING_MODEL)
+    if os.getenv("OPENAI_API_KEY"):
+        model = OpenAIEmbeddings(model=EMBEDDING_MODEL)
+    else:
+        model = GoogleGenerativeAIEmbeddings(model=EMBEDDING_MODEL)
 
     return model
