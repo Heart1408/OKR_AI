e40212d387f1161192423a9eb030172d44677d42
tamvt <vuthitam14082001@gmail.com>
2025-01-03 15:03:51 +0700

    update

--- src/chatbot.py
+++ src/chatbot.py
@@ -2,8 +2,8 @@ import streamlit as st
 import sys
 import os
 import time
-# __import__('pysqlite3')
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from dotenv import load_dotenv
 from langchain_core.tools import tool
